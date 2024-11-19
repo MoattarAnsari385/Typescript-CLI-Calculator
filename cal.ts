f75959716d0b7f5a8064c7ee29eead8d8ac32eb9
Moattar Ansari <moattaransari62@gmail.com>
2024-11-19 08:03:14 +0500

    Add files via upload

---
+++ cal.ts
@@ -0,0 +1,36 @@
+#! /user/bin/env node
+
+import inquirer from "inquirer"
+
+const answer = await inquirer.prompt([
+    {
+        name:"firstNumber",
+        type:"number",
+        message:"Enter first number:",
+    },
+    {
+        name:"secondNumber",
+        type:"number",
+        message:"Enter second number:",
+    },
+    {
+        name:"operator",
+        type:"list",
+        message:"Select one of the operator to perform operation:",
+        choices:["Addition","Subtraction","Multiplication","Division"],
+    }
+])
+if(answer.operator === "Addition"){
+    console.log(`${answer.firstNumber} + ${answer.secondNumber} = ${answer.firstNumber + answer.secondNumber}`);
+
+}else if(answer.operator === "Subtraction"){
+    console.log(`${answer.firstNumber} - ${answer.secondNumber} = ${answer.firstNumber - answer.secondNumber}`);
+    
+}else if(answer.operator === "Multiplication"){
+    console.log(`${answer.firstNumber} * ${answer.secondNumber} = ${answer.firstNumber * answer.secondNumber}`);
+    
+}else if(answer.operator === "Division"){
+    console.log(`${answer.firstNumber} / ${answer.secondNumber} = ${answer.firstNumber / answer.secondNumber}`);
+    
+}
+
